74a540f39420d9d8033dd9cbae8e525d3d845efe
RJM1999 <33182930+RJM1999@users.noreply.github.com>
2019-03-11 12:39:13 +0000

    Init commit

---
+++ aiAssignmentAmazon/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  aiAssignmentAmazon
+//
+//  Created by Ross Maider on 11/03/2019.
+//  Copyright © 2019 Ross Maider. All rights reserved.
+//
+
+#include <iostream>
+#include "Snap.h"
+
+int main(int argc, const char * argv[]) {
+    // Some comments
+    std::cout << "Hello, World!\n";
+    return 0;
+}
