73cd664316b45640e467a6aef960b9d259587f56
RJM1999 <33182930+RJM1999@users.noreply.github.com>
2019-03-11 13:01:59 +0000

    Crap

---
+++ aiAssignmentAmazon/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  aiAssignmentAmazon
+//
+//  Created by Ross Maider on 11/03/2019.
+//  Copyright © 2019 Ross Maider. All rights reserved.
+//
+
+#include <iostream>
+#include "Snap.h"
+
+int main(int argc, const char * argv[])
+{
+    // Some comments more
+    std::cout << "Hello, World!\n";
+    return 0;
+}
